bda0b5ff91cae990c992c29696793e2b8d91fe03
Danyel Murray and Sam Gespass <sam@>
2017-12-12 14:12:36 -0800

    add numbers together

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
+var balance = 0;
 
+var transfer = function(amount, balance) {
+  return amount + balance;
+}
 
 $(document).ready(function(){
   $("form#newAccount").submit(function(event){
     event.preventDefault();
     $("#balance").empty();
-    var initialDeposit = $("input.initialDeposit").val();
+    var initialDeposit = parseFloat($("input.initialDeposit").val());
     $("#balance").append("$" + initialDeposit);
+    balance = initialDeposit;
   })
   $("form#transaction").submit(function(event){
     event.preventDefault();
+    $("#balance").empty();
+    var amount = parseFloat($("input.transfer").val());
+    var newBalance = transfer(amount, balance);
+    balance = newBalance;
+    $("#balance").append("$" + newBalance)
   })
 })
