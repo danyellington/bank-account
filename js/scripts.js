c6fbdef1834035fed0ae95bf3c1ee42c88ee5fd8
Danyel Murray and Sam Gespass <danyel@>
2017-12-12 14:49:16 -0800

    keep track of number, enter name

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-var balance = 0;
-
-var transferAdd = function(amount, balance) {
-  return amount + balance;
+function Account(name, balance) {
+  this.name = name;
+  this.balance = balance;
 }
 
-var transferSubtract = function(amount, balance) {
-  return balance - amount;
+Account.prototype.transferAdd = function(amount, balance) {
+  return this.balance = amount + balance;
+}
+
+Account.prototype.transferSubtract = function(amount, balance) {
+  return this.balance = balance - amount;
 }
 
 $(document).ready(function(){
@@ -14,24 +17,27 @@ $(document).ready(function(){
     $("#balance").empty();
     var initialDeposit = parseFloat($("input.initialDeposit").val());
     $("#balance").append("$" + initialDeposit);
+    var name = "Poop";
+    newAccount = new Account(name, initialDeposit);
     balance = initialDeposit;
   })
   $("form#transaction").submit(function(event){
     event.preventDefault();
+    console.log(newAccount.balance);
     $("#balance").empty();
     var amount = parseFloat($("input.transfer").val());
     var transferType = $("input:radio[name=transaction]:checked").val();
     if (amount <= 0) {
       alert("Please enter a number greater than 0");
     } else if (transferType === "deposit") {
-      var newBalance = transferAdd(amount, balance);
-      balance = newBalance;
+      newAccount.transferAdd(amount, newAccount.balance);
+      //balance = newBalance;
     } else if (transferType === "withdrawl") {
-      var newBalance = transferSubtract(amount, balance);
-      balance = newBalance;
+      newAccount.transferSubtract(amount, newAccount.balance);
+      //balance = newBalance;
     } else {
       console.log("fuck");
     }
-    $("#balance").append("$" + balance);
+    $("#balance").append("$" + newAccount.balance);
   })
 })
